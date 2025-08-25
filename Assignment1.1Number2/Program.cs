a66f26e5ffc7ffa4d9ac4976890063357e71b14a
manue <manue@LAPTOP-NVCKR586>
2025-08-25 16:03:01 -0400

    Add project files.

---
+++ Assignment1.1Number2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Please enter a number:");
+int input1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please enter another number:");
+int input2 = Convert.ToInt32(Console.ReadLine());
+
+int sum = input1 + input2;
+Console.WriteLine($"The sum of {input1} and {input2} is {sum}");
